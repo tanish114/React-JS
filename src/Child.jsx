062b2050d841e83ec2eda778b8a574503c11040e
tanish114 <97626851+tanish114@users.noreply.github.com>
2025-11-22 16:32:26 +0530

    new

--- src/Child.jsx
+++ src/Child.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 
-const Child = (propse) => {
+const Child = ({mydata}) => {
+    let{name,age,city}=mydata
   return (
     <>
     <h1>hello child</h1>
 
-    <p>my name is {propse.myname}</p>
-    <p>my age is {propse.age}</p>
+    {/* <p>my name is {propse.myname}</p>
+    <p>my age is {propse.age}</p> */}
+
+
+  {/* <p>my name is {mydata.name}</p>
+  <p>my age is {mydata.age}</p> */}
+
+  <p>my name is {name}</p>
+  <p>my age is {age}</p>
+   
     </>
   )
 }
--- src/Propse.jsx
+++ src/Propse.jsx
@@ -2,12 +2,25 @@ import Child from "./Child"
 
 let App=()=>{
 
-    let newname="tanish singh parihar"
+    // let newname="tanish singh parihar"
+
+    let myinfo={
+        name:"tanish",
+        age:26,
+        city:"bhopal"
+
+    }
     return(
         <div>
             <h1>this is propse</h1>
             {/* <Child myname="tanish" age="25"/> */}
-            <Child myname={newname} age="25"/>
+
+
+            {/* <Child myname={newname} age="25"/> */}
+
+            <Child mydata={myinfo}/>
+
+
         </div>
     )
 }
