2dec2086ae0649988a5dd970f38f97e345b1a32f
tanish114 <97626851+tanish114@users.noreply.github.com>
2025-11-25 16:26:13 +0530

    new

--- src/Tailwind.jsx
+++ src/Tailwind.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import './Tailwind.css'
+import logo from "../public/vite.svg"
 const App = () => {
   return (
-    <div>
-      <h1>tailwind</h1>
-    </div>
+   <>
+   {/* <div className='text-red-500 border  border-blue-900 w-50 flex items-center  justify-center'>
+    hey i am tanish
+   </div> */}
+
+   <div className='text-emerald-700 border border-b-emerald-950 flex justify-between items-center '>
+
+    <img src={logo} alt="" />
+    <ul className='flex items-center justify-center gap-5'>
+      
+      <li>home</li>
+      <li>about</li>
+      <li>contact</li>
+    </ul>
+    <button type='submit' className='border ' >sign in</button>
+   </div>
+   </>
   )
 }
 
