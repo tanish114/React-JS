933a37f52d5064d6c4e1e3ab01abef809d654a73
tanish114 <97626851+tanish114@users.noreply.github.com>
2025-12-02 16:10:01 +0530

    new

---
+++ src/Hook.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hook = () => {
+    // let[any variable,function]=useState
+  return (
+    <>
+      <h1>hook</h1>
+    </>
+  )
+}
+
+export default Hook
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 // import App from './App.jsx'
 // import App from './Propse.jsx'
-import App from './Router.jsx'
+// import App from './Router.jsx'
+import App from './Hook.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
