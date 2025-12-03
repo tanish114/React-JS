a0256fd7db7cf5419274de24bd9804f19daccd11
tanish114 <97626851+tanish114@users.noreply.github.com>
2025-12-03 15:55:57 +0530

    new

--- src/Hook.jsx
+++ src/Hook.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Hook = () => {
-    // let[any variable,function]=useState
+    // let[any variable,function]=useState(initial value)  ---------syntax of usestate
+
+    let [count, setcount]=useState(0)
+    let inc=()=>{
+        setcount(count+1)
+        console.log(count);
+        
+    }
+
+    let dec=()=>{
+        setcount(count-1)
+        console.log(count);
+        
+    }
+
+    let res=()=>{
+        setcount(0)
+        console.log(count);
+        
+        
+    }
+
   return (
     <>
-      <h1>hook</h1>
+      <button onClick={inc}>increment</button> <br />
+      <button onClick={dec}>decrement</button> <br />
+      <button onClick={res}>reset</button>
+
+
+
+      <p>count: {count}</p>
     </>
   )
 }
