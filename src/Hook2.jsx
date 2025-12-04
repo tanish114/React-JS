ddc8504a768b44b791f5fb3a7f7b4c563ab09b35
tanish114 <97626851+tanish114@users.noreply.github.com>
2025-12-04 20:08:57 +0530

    new

--- src/Hook2.jsx
+++ src/Hook2.jsx
@@ -1,33 +1,42 @@
 import React, { useState } from 'react'
 
 const Hook2 = () => {
-    let [name,setname]=useState("")
-    let [city,setcity]=useState("")
+    // let [name,setname]=useState("")
+    // let [city,setcity]=useState("")
+    let [character,setcount]=useState("")
 
-    let inputchange=(e)=>{
-        // console.log(e); event
-        // console.log(e.target); which element      
-        console.log(e.target.value);
-        
-        setname(e.target.value)
-        
+    let chrcount=(e)=>{
+      
     }
 
-    let inputcity=(ev)=>{
-        console.log(ev.target.value);
-        setcity(ev.target.value)
+    // let inputchange=(e)=>{
+    //     // console.log(e); event
+    //     // console.log(e.target); which element      
+    //     console.log(e.target.value);
         
-    }
+    //     setname(e.target.value)
+        
+    // }
+
+    // let inputcity=(ev)=>{
+    //     console.log(ev.target.value);
+    //     setcity(ev.target.value)
+        
+    // }
 
   return (
     <>
-      <h1>input in form </h1> <br />
+      {/* <h1>input in form </h1> <br />
       Enter name : <input type="text" onChange={inputchange} /> 
       <br />
       your name is {name}
 <br />
       enter city: <input type="text" onChange={inputcity} /> <br />
-      your city is{city}
+      your city is{city} */}
+
+
+      enter character: <input type="text" onChange={chrcount} /> <br />
+      your character count is{}
     </>
   )
 }
