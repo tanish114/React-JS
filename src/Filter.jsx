c204c7a5fa02887a5130c80f59fcbeff0fa9098b
tanish114 <97626851+tanish114@users.noreply.github.com>
2025-12-10 15:58:54 +0530

    new

--- src/Filter.jsx
+++ src/Filter.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Filter = () => {
-
+   let[search,setsearch]=useState("")
     let mydata=["tanish","sid","Ram","Shyam"]
+
+    let filterdata=mydata.filter((e)=>{
+      return e.toLowerCase().includes(search.toLowerCase())
+    })
   return (
     
     <>
+      search <input type="text" value={search} onChange={(e)=>{setsearch(e.target.value)}} name="" id="" />
+
+      <ul>
+        {filterdata.map((e,i)=>{
+          <li key={i}>{e}</li>
       
+        })}
+      </ul>
     </>
   )
 }
