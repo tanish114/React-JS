d7961d12ab15baf7fb0f6fd19a87621591ac2c19
tanish114 <97626851+tanish114@users.noreply.github.com>
2026-01-05 20:16:18 +0530

    new

---
+++ src/Redux_toolkit.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Redux_toolkit = () => {
+  return (
+    <>
+      <h1>this is redux toolkit</h1>
+    </>
+  )
+}
+
+export default Redux_toolkit
--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,29 @@
 
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './P.jsx'
+import App from './Redux_toolkit.jsx'
+import store from './redux/store.js'
 // import App from './Propse.jsx'
 // import App from './Router.jsx'
 // import App from './Searching_in_table.jsx'
 import UserContext from './CreateContext'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
-let mydata={
-  myname:"tanish",
-  myage:25,
+// let mydata={
+//   myname:"tanish",
+//   myage:25,
   
-}
+// }
 
 createRoot(document.getElementById('root')).render(
-  <UserContext.Provider value={mydata}>
-  <BrowserRouter>
+  // <UserContext.Provider value={mydata}>
+  
+   <Provider store={store}>
      <App />
-  </BrowserRouter>
+     </Provider>
 
-  </UserContext.Provider>
+  
+
+  // </UserContext.Provider>
 )
